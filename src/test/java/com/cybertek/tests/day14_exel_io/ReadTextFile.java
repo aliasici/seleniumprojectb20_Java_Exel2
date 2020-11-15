d4ddb00d547a45a9756a8a08bbb4891d91ff7d1b
aliasici <asiciali@yahoo.com>
2020-11-15 07:48:49 -0800

    first changes

---
+++ src/test/java/com/cybertek/tests/day14_exel_io/ReadTextFile.java
@@ -0,0 +1,26 @@
+package com.cybertek.tests.day14_exel_io;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadTextFile {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        String path = "shopping_list.txt";
+        // File's constructor requires path to the file as an argument
+        File file = new File(path);
+        // check if file exist
+        System.out.println(file.exists());
+        // we can read lines of file with Scanner class
+        Scanner scanner = new Scanner(file);
+        System.out.println(scanner.nextLine());
+        System.out.println(scanner.nextLine());
+
+        while (scanner.hasNext()){
+            System.out.println(scanner.nextLine());
+        }
+
+    }
+}
